9bb27e5b68773a7258e0335fe643d2ad93f03a43
Vishal Vaish <114350565+vishal-vaish@users.noreply.github.com>
2022-12-23 16:52:59 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+class API{
+    #secure;
+    constructor(url,method='GET'){
+        this.url=url;
+        this.method=method;
+        this.#secure=url.startWith("https")
+    }
 
+    isSecure(){
+        return this.#secure
+    }
+    updateUrl(url){
+        this.url=url;
+        this.#secure=url.startWith("https")
+    }
+}
 
 module.exports = { API }
